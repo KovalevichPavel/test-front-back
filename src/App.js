276b26802dbc6c5dad5f01f6e794151a274dd04e
KovalevichPavel <kovalevichpasha1@gmail.com>
2022-09-22 21:12:02 +0300

    paragraph makes a circle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './index.css';
 
 function App() {
+  let p = document.getElementById('megaTest');
+  console.log('ss')
+
+  let $ = {
+      radius: 250, 
+      speed: 20
+  }
+  let f = 0;
+  let s = 2 * Math.PI / 180;
+  setInterval(function() {
+      f += s; 
+      p.style.left =  235 + $.radius * Math.sin(f)  + 'px'; 
+      p.style.top =   235 + $.radius * Math.cos(f) + 'px';
+  }, $.speed)
+    
   return (
     <div className="App">
-      <p>you r gay bro</p>
+      <p id='megaTest'>you r gay bro</p>
     </div>
   );
 }
